97ccb6f79c78d6503aaef6bbbdb231e9920a1a29
Steven de Oliveira <steven.de-oliveira@ocamlpro.com>
2020-04-30 11:24:57 +0200

    Unique id for add button

--- src/ui/home.ml
+++ src/ui/home.ml
@@ -373,7 +373,7 @@ let page
       events
   in
   let add_button, back_button =
-    Ui_utils.split_button "timeline-page" 8 "Add new event" "Cancel"
+    Ui_utils.split_button "timeline-add-home-button" 8 "Add new event" "Cancel"
       ~action_at_split:(fun () ->
           match Ui_utils.get_split_from_splitted "timeline-page" with
           | None -> false
