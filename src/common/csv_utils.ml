5d0a5502a7514694d2bce372eec471291a1d9afc
Steven de Oliveira <de.oliveira.steven@gmail.com>
2020-07-06 11:25:22 +0200

    Update csv parser to create unique unique_ids

--- src/common/csv_utils.ml
+++ src/common/csv_utils.ml
@@ -34,7 +34,7 @@ let title_to_csv_line ({
     Format.asprintf "\"%a\"" (Format.pp_print_list ~pp_sep:(fun fmt _ -> Format.fprintf fmt "%c" tag_separator) (fun fmt -> Format.fprintf fmt "%s")) tags
   ]
 
-let csv_line_to_title = function
+let csv_line_to_title uids = function
     start_date :: end_date :: headline :: text :: media :: group :: confidential :: ponderation :: unique_id :: rest ->
     let start_date = Utils.string_to_date start_date in
     let end_date = Utils.string_to_date end_date in
@@ -44,6 +44,13 @@ let csv_line_to_title = function
     let confidential = bool_of_string confidential in
     let ponderation = int_of_string ponderation in
 
+    let unique_id =
+      let id =
+      match unique_id with
+      | "" -> headline
+      | _ -> unique_id in
+      Utils.check_unique_id (fun s -> List.mem s uids) id
+    in
     let last_update, rest =
       match rest with
         [] -> None, []
@@ -52,7 +59,7 @@ let csv_line_to_title = function
     let tags =
       match rest with
       | [] -> []
-      | t :: _ -> String.split_on_char tag_separator t in{
+      | t :: _ -> String.split_on_char tag_separator t in {
     start_date;
     end_date;
     text;
@@ -76,17 +83,23 @@ let to_string (c : Csv.t) : string =
 
 let from_string (str : string) : title option * event list =
   let chan = Csv.of_string ~separator:',' ~has_header:true ~backslash_escape:true ~excel_tricks:true str in
-  let title = try Some (csv_line_to_title @@ Csv.next chan) with End_of_file -> None in
+  let title = try Some (csv_line_to_title [] @@ Csv.next chan) with End_of_file -> None in
   let events =
     let elist = ref [] in
+    let uids =
+      match title with
+      | None -> ref []
+      | Some title -> ref [title.unique_id] in
     let exception Not_an_event of string in
     try
       while true do
         let l = Csv.next chan in
-        let me = csv_line_to_title l in
+        let me = csv_line_to_title !uids l in
         match Utils.metaevent_to_event me with
         | None -> raise (Not_an_event (Format.asprintf "Event %a is incorrect" Utils.pp_title me))
-        | Some e -> elist := e :: !elist
+        | Some e ->
+          elist := e :: !elist;
+          uids  := e.unique_id :: !uids
       done;
       assert false
     with
