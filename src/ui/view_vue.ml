ff46a9eb621aac3ce65ad320e24051c73dbd18e6
Steven de Oliveira <de.oliveira.steven@gmail.com>
2020-08-14 16:29:34 +0200

    Fix issue 49

--- src/ui/view_vue.ml
+++ src/ui/view_vue.ml
@@ -5,5 +5,5 @@ let init token name title events =
     | None -> ()
     | Some t -> Timeline_cookies.add_timeline name t true in
   Ui_common.Ui_utils.slow_hide (Js_utils.find_component "page_content-loading");
-  Timeline_display.display_timeline title events;
+  Timeline_display.display_timeline ~view:true title events;
   Timeline_display.init_slide_from_url ~whenOnSlide:(fun _ -> ()) title events
